96ec5143f9b631f5116560325625e2205e32a6bf
Samet DEMİREZEN <73247768+Sametdmrzn@users.noreply.github.com>
2024-11-26 01:57:00 +0300

    Finding prime number

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 for(let i =1; i<=10; i++){
     for(let j = 1; j<=15; j++){
         for(let k =1; k<=10; k++){
@@ -8,3 +6,24 @@ for(let i =1; i<=10; i++){
     }
     console.log("--------------------------------------------")
 }
+//yukarıdaki kodlar 3 basamaklı sayı çarpım kodlarıdır.
+
+
+
+
+let sayi = Number(prompt("Lütfen bir sayı giriniz"));
+let sonuc =true;
+
+for(let i=2; i<=Math.floor(sayi/2); i++){
+    if(sayi%i==0){
+        //asal değildir
+        sonuc=false;
+        break;
+    }
+}
+if(sonuc){
+    alert(sayi+ "asaldır.");
+}else{
+    alert(sayi + "asal değildir!");
+}
+//yukarıdaki kodlar asal sayı bulma kodlarıdır.

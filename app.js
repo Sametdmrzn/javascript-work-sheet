85b69c8d8783455941bfc988ffee1fd322b391e3
Samet DEMİREZEN <73247768+Sametdmrzn@users.noreply.github.com>
2024-12-12 16:26:26 +0300

    Finding Perfect Number.

--- app.js
+++ app.js
@@ -76,3 +76,25 @@
 
 // Yukarıdaki kodlar küp alma kodlarıdır.
 
+
+// isPerfectNumber(28);
+
+// function isPerfectNumber(number){
+//     let toplam = 0;
+//     for (let i=2; i<=number/2 ; i++){
+//         if(number%i==0){
+//             toplam+=i;
+//         }
+//     }
+
+
+// toplam+= 1 + number;
+
+//         if(toplam==number*2){
+//             console.log("Evet bu mükmmel sayıdır.");
+//         }else{
+//             console.log("Hayır bu bir mükemmel sayı değildir.");
+//         }
+// }
+
+// Bu kodlar mükemmel sayı bulma kodlarıdırç
